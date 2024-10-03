fe87992cccd740852295f9c08faa2f53a1d7da17
Omar Nour <omar.syd.n@gmail.com>
2024-10-03 23:11:10 +0300

    Intial project setup, as per project requirments

---
+++ code/WaterSortSearch.java
@@ -0,0 +1,8 @@
+package code;
+
+public class WaterSortSearch extends GenericSearch {
+
+    public static String solve(String initialState, String strategy, boolean visualize) {
+        return "";
+    }
+}
